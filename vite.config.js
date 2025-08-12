59d6d087196bb0e1a8cf7637f5aca1e833bda7a2
Ryan W. Moore <30422172+rwm6857@users.noreply.github.com>
2025-08-12 19:32:09 -0400

    feat: enable offline support with service worker

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   base: '',
-  plugins: [react()],
-    build: { outDir: 'docs', chunkSizeWarningLimit: 1200 },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: 'src/sw.js', dest: '' }]
+    })
+  ],
+  build: { outDir: 'docs', chunkSizeWarningLimit: 1200 },
   test: {
     environment: 'happy-dom',
     setupFiles: './src/setupTests.js',
