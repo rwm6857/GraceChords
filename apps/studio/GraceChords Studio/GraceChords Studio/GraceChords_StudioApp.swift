148fdf5293b952900f99935215d13bb456d84d23
Ryan Moore <ryan.moore7272@gmail.com>
2026-07-24 16:32:55 -0400

    created XCode project for GC Studio

---
+++ apps/studio/GraceChords Studio/GraceChords Studio/GraceChords_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraceChords_StudioApp.swift
+//  GraceChords Studio
+//
+//  Created by Ryan Moore on 7/24/26.
+//
+
+import SwiftUI
+
+@main
+struct GraceChords_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
